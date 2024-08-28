d5de2a3471f20029ce8f905d718b573b9389491d
cmcdgit <cmcdgit@gmail.com>
2024-08-29 00:39:46 +0100

    Sign up/sign in cards added

--- src/features/auth/components/auth-screen.tsx
+++ src/features/auth/components/auth-screen.tsx
@@ -2,13 +2,21 @@
 
 import { useState } from "react";
 import { SignInFlow } from "../types";
+import { SignInCard } from "./sign-in-card";
+import { SignUpCard } from "./sign-up-card";
 
 export const AuthScreen = () => {
   const [state, setState] = useState<SignInFlow>("signIn");
 
   return (
     <div className="h-full flex items-center justify-center bg-[#5C3D58]">
-      <div className="md:h-auto md:w-[420px]">Auth screen</div>
+      <div className="md:h-auto md:w-[420px]">
+        {state === "signIn" ? (
+          <SignInCard setState={setState} />
+        ) : (
+          <SignUpCard setState={setState} />
+        )}
+      </div>
     </div>
   );
 };
